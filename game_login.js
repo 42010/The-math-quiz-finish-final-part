c716716f9c2a89f3317fac84b74a01877554a94b
42010 <72303709+42010@users.noreply.github.com>
2021-01-10 14:06:14 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function login() {
+    player_1_add = document.getElementById("player1_name_input").value;
+    player_2_add = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player_1_add);
+    localStorage.setItem("player2_name", player_2_add);
+
+    window.location = "game_page.html";
+}
+
